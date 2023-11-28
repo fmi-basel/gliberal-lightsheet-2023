Workaround bug inside skimage axis minor length calculation

Gastruloid_Segmentation/ExtractFeatures.py
@@ -8,11 +8,12 @@
 ##############################################################################
 
 import tifffile as tif
-from skimage.measure import regionprops_table
+from skimage.measure import regionprops
 import pandas as pd
 from skimage.measure import marching_cubes, mesh_surface_area
 import numpy as np
 import os
+from math import sqrt
 
 
 path_save = "/ExampleData/Output/"
@@ -21,18 +22,30 @@ path_data = "/ExampleData/Gastruloid/42h/"
 positions = ["Position 2"]
 
 
+prop_list = ["label","area", "major_axis_length", "solidity", "max_intensity", "mean_intensity", "min_intensity"]
 
-def surface_area_marchingcube(regionmask):
-    regionmask_int = regionmask.astype(np.uint16)
-    regionmask_int=np.pad(regionmask_int, 1, 'constant')
-    verts, faces, normals, values = marching_cubes(regionmask_int, spacing=(2.463,1,1))
-    surface_area = mesh_surface_area(verts, faces)
-    return surface_area
+def axis_minor_length_fixed(labeled_obj):
+    if labeled_obj._ndim == 2:
+        l2 = labeled_obj.inertia_tensor_eigvals[-1]
+        return 4 * sqrt(l2)
+    elif labeled_obj._ndim == 3:
+        # equivalent to _inertia_eigvals_to_axes_lengths_3D(ev)[-1]
+        ev = labeled_obj.inertia_tensor_eigvals
+        try:
+            return sqrt(10 * (-ev[0] + ev[1] + ev[2]))
+        except ValueError:
+            return 0
+    else:
+        raise ValueError("axis_minor_length only available in 2D and 3D")
 
+
+
+df_lck = pd.DataFrame()
 
 for pos in positions:
     path_raw_data = os.path.join(path_data,pos,"Stack")
-    path_seg_data = "/ExampleData/Output/Masks/"  
+    path_seg_data = os.path.join(path_save,"Masks")
+
 
     # generating file list
     file_list = sorted(os.listdir(path_raw_data))
@@ -50,14 +63,14 @@ for pos in positions:
         
         raw_data = tif.imread(os.path.join(path_raw_data,file_list_Lck[i]))
         seg_data = tif.imread(os.path.join(path_seg_data,file_list_masks[i]))
+
+        tmp_dic = {}
+        for prop in regionprops(seg_data, raw_data, spacing = (2.463,1,1)):
+
+            tmp_dic.update({p: prop[p] for p in prop_list})
+            tmp_dic.update({"minor_axis_length": axis_minor_length_fixed(prop)})
         
-        
-        mem_features = regionprops_table(seg_data, raw_data, spacing = (2.463,1,1), properties = ["label","area", "major_axis_length", "minor_axis_length", "solidity", "max_intensity", "mean_intensity", "min_intensity"], extra_properties=[surface_area_marchingcube])
-        mem_features = pd.DataFrame(mem_features)
-        
-        if "df_lck" not in locals():
-            df_lck = pd.DataFrame(mem_features)
-        else:
+            mem_features = pd.DataFrame(tmp_dic, index = [0])
             df_lck = pd.concat([df_lck, mem_features], axis = 0)
-        
+
 df_lck.to_csv(path_save + "df_gastruloid.csv")